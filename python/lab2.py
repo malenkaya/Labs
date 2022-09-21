6f38d00df85f6b3f7e5f99c41f316faf00ed205f
Dilnaz <74405096+malenkaya@users.noreply.github.com>
2022-09-21 13:27:20 +0600

    Python

---
+++ python/lab2.py
@@ -0,0 +1,12 @@
+# Функциональное программирование Калиева Дильназ
+
+firstname = "Dilnaz"
+lastname = "Kalieva"
+year = 2002
+
+if year < 2000:
+    print("Вы меньше 2000 года")
+elif year > 2000 and year < 1995 :
+    print("Вы больше 2000 года")
+else:
+    print("Вы 2000 года")
