74240f3faecc2a5fc94013667a8f54fa368ec511
Dilnaz <74405096+malenkaya@users.noreply.github.com>
2022-09-21 11:38:37 +0600

    Lab_1

--- python/lab1.py
+++ python/lab1.py
@@ -1,1 +1,13 @@
-print("Hello");
+print("salem alem")
+print("Kalieva Dilnaz first program!!!")
+print(2+1)
+a = 24 + 2 # Қосу
+b = 2 - 20 # Азайту
+c = 12 * 6 # Көбейту
+d = 12/2 # Бөлу
+e = 4**2 # Дәрежелеу e = 4*4
+print("a-->", a)
+print("b-->", b)
+print("c-->", c)
+print("d-->", d)
+print("e-->", e)
